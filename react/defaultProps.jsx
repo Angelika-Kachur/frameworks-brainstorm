2dbc9534d1a99d30690b0d58502ac4255813cb38
Angelika Kachur <angelika.kachur@gmail.com>
2019-07-26 15:12:06 +0300

    default Props and override

--- react/defaultProps.jsx
+++ react/defaultProps.jsx
@@ -1,11 +1,16 @@
-const ShoppingCart = (props) => {
-  return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-    </div>
-  )
+const Items = props => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
 };
 
-ShoppingCart.defaultProps = {
-  items: 0 //if undefined -> 0, if null -> null
+Items.defaultProps = {
+  quantity: 0 //if undefined -> 0, if null -> null
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items quantity={10} />;
+  }
 }
