967a6bd12ce3e708fadc3886c16f42a207c28a18
Angelika Kachur <angelika.kachur@gmail.com>
2019-07-28 00:11:35 +0300

    merge

--- react/PropTypesChecking.jsx
+++ react/PropTypesChecking.jsx
@@ -2,6 +2,10 @@ const Items = props => {
   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
 };
 
+Items.defaultProps = {
+  quantity: 0 //if undefined -> 0, if null -> null
+};
+
 class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
