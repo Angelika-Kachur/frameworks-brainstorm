f25471c52874f5ab7fd9b62ac5b934ad73a5b8be
Angelika Kachur <angelika.kachur@gmail.com>
2019-07-31 23:57:19 +0300

    setState method

---
+++ react/setState.jsx
@@ -0,0 +1,28 @@
+//Way to change the component's state.
+//React provides a method for updating component state
+//called setState. You call the setState method within
+//your component class like so: this.setState(),
+//passing in an object with key-value pairs.
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Initial State"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      name: "React Rocks!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+}
