d753602dd9235f74daad264d84b97c32e648db9b
Angelika Kachur <angelika.kachur@gmail.com>
2019-08-02 23:04:13 +0300

    use State to toggle element view

---
+++ react/useStateToToggleElement.jsx
@@ -0,0 +1,39 @@
+//One example is to monitor the status of a value,
+//then render the UI conditionally based on this value.
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+  toggleVisibility() {
+    if (this.state.visibility == true) {
+      this.setState({
+        visibility: false
+      });
+    } else {
+      this.setState({
+        visibility: true
+      });
+    }
+  }
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+}
