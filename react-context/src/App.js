8e551b6e68fe79bc2f57111ba8f9a94f16c48a20
Angelika Kachur <angelika.kachur@gmail.com>
2019-10-07 14:26:32 +0300

    deleted extra

--- react-context/src/App.js
+++ react-context/src/App.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 import Items from "./items";
 
-export const Interval = () => {
-  useEffect(() => {
-   const time = setInterval(() => {
-      console.log("ticking");
-    }, 1000);
-    return () => {
-    clearInterval(time)
-    };
-  },[]);
-  return <h1>I AM AN INTERVAL COMPONENT</h1>;
-};
 function App() {
-  {/*const [modal, setModal] = useState(true);*/}
   return (
     <div className="App">
       <Items />
-      {/* <button onClick={()=>
-        setModal(false)
-        }>Close Modal</button>
-      <form action="post">
-        <input type="text" name="query" id="query" placeholder="QUERY" />
-      </form>
-      {modal && <Interval />}
-     */}
      </div>
   );
 }
